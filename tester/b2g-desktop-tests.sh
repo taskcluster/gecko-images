ae85d6fc9ca9b6d744ddd134a86a9b5e5c204420
Geoff Brown <gbrown@mozilla.com>
2014-07-25 15:19:55 -0600

    Bug 1027249: Add support for b2g emulator tests

--- tester/b2g-desktop-tests.sh
+++ tester/b2g-desktop-tests.sh
@@ -5,8 +5,6 @@
 ### Check that we are running as worker
 test `whoami` == 'worker';
 
-CONFIG_FILE=/home/worker/desktop_automation_config.py
-
 # Fetch URLs for input binary and test zip file
 #INSTALLER_URL=`./fetch-artifacts.py $TARGET_TASK -u target.linux-x86_64.tar.bz2`;
 #TEST_URL=`./fetch-artifacts.py $TARGET_TASK -u target.tests.zip`;
@@ -16,9 +14,8 @@ BUILD_ROOT=http://172.17.42.1/LocalB2G
 INSTALLER_URL=$BUILD_ROOT/b2g-31.0a1.en-US.linux-x86_64.tar.bz2
 TEST_URL=$BUILD_ROOT/b2g-31.0a1.en-US.linux-x86_64.tests.zip
 
-# Run tests for b2g-desktop under xvfb
-xvfb-run -s "-screen 0 800x1000x24" \
-  python ./mozharness/scripts/b2g_desktop_unittest.py \
+# Run tests for b2g-desktop
+python ./mozharness/scripts/b2g_desktop_unittest.py \
   --no-read-buildbot-config \
   --config-file /home/worker/b2g-desktop-config.py \
   --installer-url $INSTALLER_URL \
